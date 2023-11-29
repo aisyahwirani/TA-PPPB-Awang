dc58969108e3e0707cfc29299d6a195ccaed9a70
aisyahwirani <102150442+aisyahwirani@users.noreply.github.com>
2023-11-30 02:03:47 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div>
-      <Meta title="Foody" />
+      <Meta title="Resep Kostan" />
 
       <Header />
 
