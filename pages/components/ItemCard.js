155a5a650823649e4f78030032fd576c29b93cce
aisyahwirani <102150442+aisyahwirani@users.noreply.github.com>
2023-11-30 17:06:11 +0700

    Update ItemCard.js

--- pages/components/ItemCard.js
+++ pages/components/ItemCard.js
@@ -2,20 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function ItemCard(props) {
-  try {
+  if (!props.data || !props.data.id) {
+    console.log('null')
+    return null;
+  }
+
     return (
-      <div className="lg:w-1/4 md:w-1/2 p-4 w-full ">
-        <Link
-          href={`/detail/${props.data.id}`}
-          className="block relative h-48 rounded overflow-hidden"
-        >
-          <Image
-            alt={data.name ?? ''}
-            className="object-cover object-center w-full h-full block"
-            layout="fill"
-            objectFit="cover"
-            src={props.data.thumbnail_url}
-          />
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <Link href={`/detail/${props.data.id}`} className="block relative h-48 rounded overflow-hidden">
+            <Image
+              alt={props.data.name ?? ""}
+              className="object-cover object-center w-full h-full block"
+              layout="fill"
+              objectFit="cover"
+              src={props.data.thumbnail_url}
+            />
         </Link>
         <div className="mt-4">
           <h3 className="text-gray-800 text-xs tracking-widest title-font mb-1">
@@ -28,7 +29,4 @@ export default function ItemCard(props) {
         </div>
       </div>
     );
-  } catch (e) {
-    console.log(e);
-  }
 }
