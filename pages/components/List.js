e1d048a7ecca92a042a9847d732560456cad5583
aisyahwirani <102150442+aisyahwirani@users.noreply.github.com>
2023-11-30 17:07:01 +0700

    Update List.js

--- pages/components/List.js
+++ pages/components/List.js
@@ -4,13 +4,29 @@ import ItemCard from './ItemCard';
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '7469b2abb6mshdf620d7cc6a5c7ep1698cajsn80d489e268cc',
+        'X-RapidAPI-Key': '9d29623f5cmshda1be642e86632bp1772a1jsn18af603d3d95',
         'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
     }
 };
 
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+// export const getStaticProps = async()=>{
+//     const API_URL = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes"
+//     const request = await fetch(API_URL,options)
+//     const data = await request.json()
+//     console.log(data);
+//     return {props : {data}}
+// }
+
 export default function List() {
-    const [data, setData] = useState({results: []})
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
@@ -18,22 +34,25 @@ export default function List() {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
-                return response.json();
+                console.log(response);
+                return response.json()
+            }).then(data=>{
+                console.log(data)
+                setData(data)
             })
-            .then(response => {
-                console.log('API Response:', response);
-                setData(response);
+            .catch(err => console.error('Fetch error:', err))
+            .finally(() => {
+                setLoading(false)
             })
-            .catch(err => console.error('Fetch error:', err));
     }, []);
-    
+
 
     return (
         <section className="text-gray-600 body-font bg-green-50">
             <div className="container px-5 py-8 mx-auto">
                 <div className="flex flex-wrap -m-4">
                     {   
-                        data.results.length === 0 ? (<p className="mx-auto text-center font-bold">Loading...</p>) :
+                          loading ? (<p className="mx-auto text-center font-bold">Loading...</p>) :
                         data.results.map((item, index) => (
                             <ItemCard key={index} data={item} />
                         ))
